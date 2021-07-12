2fb2ce155b45e2e9f39a7bee78a87a2b3fff2e0f
A. Muh. Fakhry Mubarak <fakhrymubarak@gmail.com>
2021-07-12 18:16:23 +0700

    reafctore: update gitignore

--- app/config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define('BASE_URL', "http://localhost/srms/public/");
-
-define('DB_HOST', 'localhost');
-define('DB_USER', 'caelum');
-define('DB_PASS', 'caelum135.');
-define('DB_NAME', 'sistem_rapor_pweb');
